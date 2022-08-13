1f653e9a14893d50ebd2c8edd272d82de36941a5
alexvolt <alexvolt@LOCALCOMP>
2022-08-12 23:07:02 -0400

    cors fixed

--- aspnetserver/Program.cs
+++ aspnetserver/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddCors(options =>
             builder
             .AllowAnyMethod()
             .AllowAnyHeader()
-            .WithOrigins("http://localhost:3000", "https://appname.azurestaticapps.net");
+            .WithOrigins("http://localhost:3000", "https://proud-bush-060247b10.1.azurestaticapps.net");
         });
 });
 
